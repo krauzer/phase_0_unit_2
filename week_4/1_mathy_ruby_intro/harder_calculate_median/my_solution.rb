788692cf45440b3ee4359708c232c958645785a4
Dylan Krause <dykrause@gmail.com>
2014-05-12 18:48:38 -0400

    added pseudocode

--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -9,8 +9,16 @@
 # 1. Pseudocode
 
 # What is the input?
+#Input: array of numbers (floats or integers) or strings 
 # What is the output? (i.e. What should the code return?)
+#Output: Median value, either a number (float or integer) or a string
 # What are the steps needed to solve the problem?
+#DEFINE a method, median
+#SET variable, middle, to nil
+#SORT argument array in ascending order
+#DETERMINE middle value of argument array using division 
+#AVERAGE the two middle values if the argument array is of even length 
+#SET middle variable to middle value for odd arrays or averaged middle for even array 
 
 
 # 2. Initial Solution
