14453c0726e98c14b689f48ffd927eab9bbda986
Dylan Krause <dykrause@gmail.com>
2014-05-12 15:44:24 -0400

    Added initial Code

--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -13,14 +13,35 @@
 # What is the output? (i.e. What should the code return?)
 	#Output: array of number/s consisting of the most frequent elements in the input array
 # What are the steps needed to solve the problem?
-	#INIT a new hash, uniques, that is empty
+	#INIT a new hash, vals, that is empty
 	#INIT a new empty array, mode_vals, to be returned
-	#ITERATE over inputed array and add element as key, occurences as value
-	#PUSH keys with highest values into mode_vals array
+	#ITERATE over inputed array: 
+		#ADD element as key IF not in vals and SET value to 1
+		#INCREMENT value if element alreay a key
+	#PUSH keys with highest values into mode_vals array:
+		#INSERT values in array
+		#RETURN max value from that array
+		#PUSH keys with max value in mode_vals
 	#RETURN mode_vals
 
 # 2. Initial Solution
-
+def mode(array)
+	vals = {}
+	mode_vals = []
+	array.each {|x|
+		if vals.include? x 
+			vals[x] += 1
+		else
+			vals[x] = 1
+		end
+	}
+	#hash.values puts values in array. array.max returns the max!
+	max = vals.values.max
+	vals.each {|key,value| 
+		mode_vals.push(key) if value == max
+	}
+	mode_vals
+end
 
 
 
