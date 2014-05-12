654711b459f1cf3a07dc55fe03ddf6c76bc82593
Dylan Krause <dykrause@gmail.com>
2014-05-12 15:13:13 -0400

    Added pseudocode

--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -9,9 +9,15 @@
 # 1. Pseudocode
 
 # What is the input?
+	#Input: array of numbers (floats, integers) or strings. 
 # What is the output? (i.e. What should the code return?)
+	#Output: array of number/s consisting of the most frequent elements in the input array
 # What are the steps needed to solve the problem?
-
+	#INIT a new hash, uniques, that is empty
+	#INIT a new empty array, mode_vals, to be returned
+	#ITERATE over inputed array and add element as key, occurences as value
+	#PUSH keys with highest values into mode_vals array
+	#RETURN mode_vals
 
 # 2. Initial Solution
 
@@ -23,3 +29,11 @@
 
 
 # 4. Reflection 
+=begin
+	 
+
+	I've rethought my pseudocoding strategy because the page gets less readable with so many comments. Also, I think DBC wants us to commit at 
+	each stage, so I shouldn't do all of them at the same time. 
+
+	
+=end
