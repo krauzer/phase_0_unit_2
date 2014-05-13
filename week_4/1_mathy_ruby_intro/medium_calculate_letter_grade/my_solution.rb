43fda9d8351148cc40b9e250b92a64e5c1e329bc
Dylan Krause <dykrause@gmail.com>
2014-05-12 20:14:09 -0400

    Solution/Reflection to letter_grade method

--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
@@ -43,8 +43,41 @@ end
 
 
 # 3. Refactored Solution
+#Intitial code doesn't need refactoring
+def get_grade(array)
+	average = array.reduce(:+)/array.length
+	case average
+		when 90...100
+			"A"
+		when 80..90
+			"B"
+		when 70..80
+			"C"
+		when 60..70
+			"D"
+		when 0..60
+			"F"
+		else
+			"ERROR"
+	end
+end
 
 
 
 # 4. Reflection 
+=begin
+	
+	This is the last mathy problem for me to do. I basically have the same reflection as the last three. 
+	I looked up how to sum and array and found the reduce method for an enumerable. It seems like a 
+	very useful method. 
 
+	I was going to use and array of letters and draw from them using an index but I don't know if it would 
+	have improved the code at all, so I use the situation to test of a switch statement. 
+
+	I've been working on the learning competencies and I hope I've improved with them. I did all four exercises in the 
+	release because I thought they were good practice. 
+
+	This was similar to the excercise on socrates though. 
+
+	
+=end
