9c443f7bd15508becd3c119cde7ca797423c8d1a
Dylan Krause <dykrause@gmail.com>
2014-05-13 12:25:52 -0400

    Pseudocode and Initial Solution for solo

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -8,16 +8,39 @@
 # 1. Pseudocode
 
 # What is the input?
+#Input: Fixnum
 # What is the output? (i.e. What should the code return?)
+#Output: String
 # What are the steps needed to solve the problem?
-
+#DEFINE method, separate_coma
+#REDEFINE inputed number, as a string and reverse it, name it output
+#RETURN reversed output if less than four characters 
+#DETERMINE number of commas needed by examining the length of output
+#INSERT comma at fourth index of output
+#RETURN reverse output if length is fewer than seven characters
+#FOR range of number of commas left in total commas:
+	#INSERT comma at every fourth index 
+#RETURN reversed output
 
 # 2. Initial Solution
 
+def separate_comma(number)
+	output = number.to_s.reverse
+	return number.to_s if output.length < 4
+	len = output.length 
+	commas = (len-1)/3
+	marker = 3
+	output.insert(marker, ',')
+	return output.reverse! if output.length < 7
+	(2..commas).each{|x| marker += 4; output.insert(marker, ',') }
+	output.reverse!
+
+end
 
 
 
 # 3. Refactored Solution
+
 
 
 
