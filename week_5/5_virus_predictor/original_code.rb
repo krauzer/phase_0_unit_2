42b24a28f5fce9f45566655cd58323448ffa6e9c
Dylan Krause <dykrause@gmail.com>
2014-05-23 23:31:53 -0400

    Virus Predictor Analysis

--- week_5/5_virus_predictor/original_code.rb
+++ week_5/5_virus_predictor/original_code.rb
@@ -1,14 +1,15 @@
 # U2.W5: Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # EXPLANATION OF require_relative
-#
+# require_relative loads the file 'state_data' that is relative to this file 
 #
 require_relative 'state_data'
 
 class VirusPredictor
 
+  #Initializes the class VirusPredictor class with indicated properties 
   def initialize(state_of_origin, population_density, population, region, regional_spread)
     @state = state_of_origin
     @population = population
@@ -17,14 +18,20 @@ class VirusPredictor
     @next_region = regional_spread
   end
 
+  #Runs private predicted_deaths and speed_of_spread methods to illustrate the the virus effects
+  #The instance variables examined have a scope of the instance object 
   def virus_effects  #HINT: What is the SCOPE of instance variables?
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private  #what is this?  what happens if it were cut and pasted above the virus_effects method
+            #moving the private label would make the virus_effects method private, so it couldn't be called 
+            #by a user
 
-  def predicted_deaths(population_density, population, state)
+  #A private method that can only be used in the VirusPredictor class. 
+  #predicted_death methods predict death based on density. Higher density translates to more relative deaths
+  def predicted_deaths
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
     elsif @population_density >= 150
@@ -41,7 +48,8 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  #speed_of_spread method predicts spread of virus based on density. Higher density translates to a quicker spread
+  def speed_of_spread #in months
     speed = 0.0
 
     if @population_density >= 200
@@ -62,6 +70,16 @@ class VirusPredictor
 
 end
 
+
+
+#RELEASE 4: Report for all 50 States 
+
+STATE_DATA.map{|state, info_hash| 
+  VirusPredictor.new(state, 
+                           STATE_DATA[state][:population_density],
+                           STATE_DATA[state][:population],
+                           STATE_DATA[state][:region],
+                           STATE_DATA[state][:regional_spread]).virus_effects}
 #=======================================================================
 
 # DRIVER CODE
@@ -79,3 +97,28 @@ california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population], STATE_DATA["Alaska"][:region], STATE_DATA["Alaska"][:regional_spread]) 
 alaska.virus_effects
+
+
+
+
+#REFLECTION 
+
+=begin
+  I enjoyed working on this challenge because I think it reflects the problem one may face when dealing 
+  with set code or legacy code. It is obviously very bulky, but it is set up to handle a specific object. 
+
+  This problem was about private/public methods and refactoring. The private/public distinction was one that I easily 
+  picked up on. The refactoring was harder. 
+
+  Mapping to al 50 states took a lot of code. I thought about refactoring the original class initializer because it took in 
+  5 parameters, but that would depend on the source format never changing, which I thought I couldn't guarantee. 
+  Also I thought about refactoring the private methods, but a case statement wouldn't work. I think the entire code needs an 
+  overhaul. 
+
+  While not dry, I don't think the code is terrible for running through 50 states. In a real situation, I think you would develop
+  an national virus predictor and keep the state one separate. I know ruby is about keeping things dry, but you also don't want to lump
+  important analysis features in one whole class. 
+
+  I'm looking forward to more challenges like this one in the future. 
+  
+=end
